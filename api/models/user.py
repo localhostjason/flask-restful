1b4c930b080a20873c16ab2d61c89479efb139ef
zhangjie <zhangjie@tcxa.com.cn>
2023-01-10 13:56:51 +0800

    fix: itsdangerous.TimedJSONWebSignatureSerializer 废弃 换用 authlib

--- api/models/user.py
+++ api/models/user.py
@@ -1,8 +1,12 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
+# from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
+# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from authlib.jose import jwt, JoseError
 
 from .. import db
+
+import datetime
 
 
 class User(db.Model):
@@ -27,17 +31,29 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self._password, password)
 
-    def generate_auth_token(self, expiration=3600 * 8):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+    def generate_auth_token(self):
+        """生成用于邮箱验证的JWT（json web token）"""
+        # 签名算法
+        header = {'alg': 'HS256'}
+        # 用于签名的密钥
+        key = current_app.config['SECRET_KEY']
+        # 待签名的数据负载
+        now = datetime.datetime.now()
+        exp = now + datetime.timedelta(hours=8)
+        data = {'id': self.id, 'exp': exp}
+        # data.update(**kwargs)
 
-        return s.dumps({'id': self.id})
+        return jwt.encode(header=header, payload=data, key=key)
+
+        # s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        # return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
+        key = current_app.config['SECRET_KEY']
 
         try:
-            data = s.loads(token)
+            data = jwt.decode(token, key)
         except:
             return None
         return User.query.get(data['id'])
